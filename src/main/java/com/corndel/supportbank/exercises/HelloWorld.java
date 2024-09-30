63db79c9f76ca406e1081f5dbb546b68caa8e2da
mik-rzo <michael.panong3@gmail.com>
2024-09-30 10:59:51 +0100

    completed day 1 exercise 2

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -1,8 +1,20 @@
 package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 // import picocli.CommandLine.Parameters;
 
-public class HelloWorld {
+@Command(name = "hello")
+public class HelloWorld implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        CommandLine cli = new CommandLine(new HelloWorld());
+        int exitCode = cli.execute(args);
+        System.exit(exitCode);
+    }
 }
