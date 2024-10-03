05764a44b92ead3598e99a7b8e87dccd4a86a0f6
mik-rzo <michael.panong3@gmail.com>
2024-10-03 15:35:45 +0100

    refactored currency list service

--- src/main/java/com/corndel/supportbank/services/CurrencyListService.java
+++ src/main/java/com/corndel/supportbank/services/CurrencyListService.java
@@ -19,21 +19,10 @@ public class CurrencyListService implements Runnable {
         OpenExchangeRateAPI api = new OpenExchangeRateAPI("latest.json");
         String json = api.getRequest();
 
-        ObjectMapper objectMapper = new ObjectMapper();
         try {
-            var tree = objectMapper.readTree(json);
-
-            // retrieve timestamp from json
-            Long timestamp = tree.get("timestamp").asLong();
-            String formattedTimestamp = CurrencyUtils.formatTimestamp(timestamp);
-
-            // retrieve currency rates from json
-            JsonNode ratesNode = tree.get("rates");
-
-            // convert jsonNode to a map
-            Map<String, Double> rates = objectMapper.convertValue(ratesNode, Map.class);
-
-            System.out.println("As of " + formattedTimestamp + ", 1 USD is equivalent to:");
+            // retrieve exchange rates from string json
+            Map<String, Double> rates = CurrencyUtils.exchangeRates(json);
+            System.out.println("1 USD is equivalent to:");
             for (Map.Entry<String, Double> entry : rates.entrySet()) {
                 System.out.println(entry.getKey() + ": " + entry.getValue());
             }
--- src/main/java/com/corndel/supportbank/utils/CurrencyUtils.java
+++ src/main/java/com/corndel/supportbank/utils/CurrencyUtils.java
@@ -1,12 +1,30 @@
 package com.corndel.supportbank.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Map;
 
 public class CurrencyUtils {
 
     public static String formatTimestamp(Long timestamp) {
         Date date = new Date(timestamp * 1000);
         return new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date);
+    }
+
+    public static Map<String, Double> exchangeRates(String json) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        var tree = objectMapper.readTree(json);
+
+        // retrieve currency rates from json
+        JsonNode ratesNode = tree.get("rates");
+
+        // convert jsonNode to a map
+        Map<String, Double> rates = objectMapper.convertValue(ratesNode, Map.class);
+
+        return rates;
     }
 }
